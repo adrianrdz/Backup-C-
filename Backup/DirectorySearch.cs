62b593cf2d7889e03efcd98605a7f853787a9dc9
Francisco Rodriguez Cabral <franciscordz194@gmail.com>
2018-07-13 14:34:33 -0600

    Initial commit

---
+++ Backup/DirectorySearch.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic;
+namespace Backup
+{
+    class DirectorySearch
+    {
+
+        private string sourceDir;
+        private string backupDir;
+        private string extension;
+        //private DirectoryInfo src;
+        //private DirectoryInfo bck;
+        private string[] subDirs;
+
+        public string Source
+        {
+            get { return sourceDir; }
+            set
+            { 
+                sourceDir = value;
+            }
+        }
+
+        public String Backup
+        {
+            set 
+            {
+                backupDir = value; 
+            }
+        }
+
+        public string FileExtension
+        {
+            set { extension = "*."+ value; }
+        }
+
+        public void moveFiles(string path)
+        {
+
+            string[] dirs = Directory.GetDirectories(path);
+            var files = Directory.EnumerateFiles(path, extension);
+            if (dirs.Length > 0 || files != null)
+            {
+                if (!path.Equals(backupDir))
+                {
+                    foreach (string curr in files)
+                    {
+                       
+                        string fileName = curr.Substring(path.Length + 1);
+                        string source = System.IO.Path.Combine(path, fileName);
+                        string dest = System.IO.Path.Combine(backupDir, fileName);
+                        
+                        //Directory.Move(curr, Path.Combine(backupDir, fileName));
+                        //System.IO.File.Copy(source, dest, true);
+                        FileSystem.CopyDirectory(source, dest, UIOption.AllDialogs);
+                    }
+                }
+                foreach (string s in dirs)
+                {
+                    moveFiles(s);
+                }
+            }
+         //   String []rootSubDirs = Directory.GetDirectories(sourceDir);
+            //Console.WriteLine(rootSubDirs[0]);
+          //  foreach (string subDir in rootSubDirs) 
+         //   {
+         //       String[] subDirSubs = Directory.GetDirectories(subDir);
+         //   }
+         
+        }
+
+
+        public void moveFiles1() 
+        {
+            List<IEnumerable<string>> f = null;
+            foreach (string folder in subDirs) {
+                f.Add(Directory.EnumerateFiles(sourceDir +"" +folder, extension));
+
+            }
+            var files = Directory.EnumerateFiles(sourceDir, extension);
+
+            foreach (string curr in files)
+            {
+                string fileName = curr.Substring(sourceDir.Length + 1);
+                Directory.Move(curr, Path.Combine(backupDir, fileName));
+            }
+        }
+    }
+}
---
+++ Backup/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Backup
+{
+    public partial class Form1 : Form
+    {
+        DirectorySearch dir = new DirectorySearch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string extension = comboBox1.SelectedItem.ToString().ToLower();
+            dir.FileExtension = extension;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using(var browseDir = new FolderBrowserDialog())
+            {
+                browseDir.ShowDialog();
+                textBox1.Text = String.Format("{0}",browseDir.SelectedPath);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dir.Source = textBox1.Text;
+            Console.WriteLine(dir.Source);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var browseDir = new FolderBrowserDialog())
+            {
+
+                browseDir.ShowDialog();
+                textBox2.Text = String.Format("{0}", browseDir.SelectedPath);
+                string path = String.Format("{0}", browseDir.SelectedPath);
+                DirectoryInfo d = null;
+                try
+                {
+                    d = new DirectoryInfo(String.Format("{0}", browseDir.SelectedPath));
+                    if (d.Exists && !Directory.EnumerateFileSystemEntries(path).Any())
+                    {
+                        dir.Backup = path;
+                        Console.WriteLine("Empty!!"); //Debugging
+                    }
+                    else
+                        Console.WriteLine("Full!!");//Debugging
+                }
+                catch (System.ArgumentException) 
+                {
+                    d = null;
+                }
+                
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //dir.getSubDirectories();
+            dir.moveFiles(dir.Source);
+        }
+    }
+}
